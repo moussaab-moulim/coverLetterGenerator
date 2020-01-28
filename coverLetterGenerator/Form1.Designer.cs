d8fbb79ab17492b85c4d36ffb4163e990e5ab18d
moussaab moulim <moussaabmma@gmail.com>
2020-01-28 16:09:50 +0000

    Add project files.

---
+++ coverLetterGenerator/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace coverLetterGenerator
+{
+    partial class CLG
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGVData = new System.Windows.Forms.DataGridView();
+            this.company = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.position = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.recipient = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.finish = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BtnGenerate = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGVData
+            // 
+            this.DGVData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.company,
+            this.position,
+            this.recipient,
+            this.finish});
+            this.DGVData.Location = new System.Drawing.Point(64, 50);
+            this.DGVData.Name = "DGVData";
+            this.DGVData.Size = new System.Drawing.Size(443, 285);
+            this.DGVData.TabIndex = 0;
+            // 
+            // company
+            // 
+            this.company.HeaderText = "company";
+            this.company.Name = "company";
+            // 
+            // position
+            // 
+            this.position.HeaderText = "position";
+            this.position.Name = "position";
+            // 
+            // recipient
+            // 
+            this.recipient.HeaderText = "recipient";
+            this.recipient.Name = "recipient";
+            // 
+            // finish
+            // 
+            this.finish.HeaderText = "finish";
+            this.finish.Name = "finish";
+            // 
+            // BtnGenerate
+            // 
+            this.BtnGenerate.Location = new System.Drawing.Point(223, 360);
+            this.BtnGenerate.Name = "BtnGenerate";
+            this.BtnGenerate.Size = new System.Drawing.Size(109, 40);
+            this.BtnGenerate.TabIndex = 1;
+            this.BtnGenerate.Text = "generate";
+            this.BtnGenerate.UseVisualStyleBackColor = true;
+            this.BtnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(403, 428);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(169, 13);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Developed by moulimmoussaab.ml";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // CLG
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 450);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.BtnGenerate);
+            this.Controls.Add(this.DGVData);
+            this.Name = "CLG";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cover Letters Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.DGVData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGVData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn company;
+        private System.Windows.Forms.DataGridViewTextBoxColumn position;
+        private System.Windows.Forms.DataGridViewTextBoxColumn recipient;
+        private System.Windows.Forms.DataGridViewTextBoxColumn finish;
+        private System.Windows.Forms.Button BtnGenerate;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ coverLetterGenerator/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using Word = Microsoft.Office.Interop.Word;
+using System.Collections.ObjectModel;
+
+namespace coverLetterGenerator
+{
+    public partial class CLG : Form
+    {
+        public CLG()
+        {
+            InitializeComponent();
+        }
+
+        private void FindAndReplace(Word.Application wordApp, object ToFindText, object replaceWithText)
+        {
+            object matchCase = true;
+            object matchWholeWord = true;
+            object matchWildCards = false;
+            object matchSoundLike = false;
+            object nmatchAllforms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiactitics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object read_only = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = 1;
+
+            bool replaced = wordApp.Selection.Find.Execute(ref ToFindText,
+                ref matchCase, ref matchWholeWord,
+                ref matchWildCards, ref matchSoundLike,
+                ref nmatchAllforms, ref forward,
+                ref wrap, ref format, ref replaceWithText,
+                ref replace, ref matchKashida,
+                ref matchDiactitics, ref matchAlefHamza,
+                ref matchControl);
+            Console.WriteLine(replaced);
+
+        }
+
+        //Creeate the Doc Method
+        private void CreateWordDocument(object location)
+        {
+            
+            foreach (DataGridViewRow row in DGVData.Rows)
+            {
+                if (row.Index == DGVData.RowCount-1) { break; }
+                Word.Application wordApp = new Word.Application();
+                object missing = Missing.Value;
+                Word.Document myWordDoc = null;
+                object filename = ((string)location) + "CoverTemp.docx";
+                object SaveAs= ((string)location) + "Moussab_MOULIM_CoverLetter_"+ row.Cells[0].Value.ToString().Replace(" ","_")+ ".docx";
+
+                if (File.Exists((string)filename))
+                {
+                    object readOnly = false;
+                    object isVisible = false;
+                    wordApp.Visible = false;
+
+                    myWordDoc = wordApp.Documents.Open(ref filename, ref missing, ref readOnly,
+                                            ref missing, ref missing, ref missing,
+                                            ref missing, ref missing, ref missing,
+                                            ref missing, ref missing, ref missing,
+                                            ref missing, ref missing, ref missing, ref missing);
+                    myWordDoc.Activate();
+
+                    //find and replace
+                    this.FindAndReplace(wordApp, "<recipient>", row.Cells[2].Value.ToString());
+                    this.FindAndReplace(wordApp, "<position>", row.Cells[1].Value.ToString());
+                    this.FindAndReplace(wordApp, "<company>", row.Cells[0].Value.ToString());
+                    this.FindAndReplace(wordApp, "<finish>", row.Cells[3].Value.ToString().Equals("y")?"Your sincerely":"Your faithfully");
+                }
+                else
+                {
+                    MessageBox.Show("File not Found!. Please make sure the template is in the same directory as the program.");
+                }
+
+                //Save as
+                myWordDoc.SaveAs2(ref SaveAs, ref missing, ref missing, ref missing,
+                                ref missing, ref missing, ref missing,
+                                ref missing, ref missing, ref missing,
+                                ref missing, ref missing, ref missing,
+                                ref missing, ref missing, ref missing);
+
+                myWordDoc.Close();
+                wordApp.Quit();
+                
+
+                
+            }
+            MessageBox.Show("Cover Letters Created!");
+
+        }
+
+
+        private void BtnGenerate_Click(object sender, EventArgs e)
+        {
+            CreateWordDocument(Application.StartupPath + @"\");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Navigate to a URL.
+            System.Diagnostics.Process.Start("http://moussaab-moulim.github.io/");
+        }
+    }
+}
